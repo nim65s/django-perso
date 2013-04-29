0af6d8e04d9e182fb6e684ef949dd90a226d39e3
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-04-29 23:24:41 +0200

    passage de profil de cine à perso

--- perso/urls.py
+++ perso/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from perso.views import login_view, logout_view, about, home
+from perso.views import *
 
 admin.autodiscover()
 
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
 
         url(r'^accounts/login', login_view, name='login'),
         url(r'^accounts/logout', logout_view, name='logout'),
+        url(r'^accounts/profil', profil, name='profil'),
 
         url(r'^when/', include('when.urls', namespace="when")),
         url(r'^cine/', include('cine.urls', namespace="cine")),
