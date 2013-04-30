77614a2ed4fe012fd7d26df377d57793138366f4
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-04-30 13:47:40 +0200

    logging console

--- perso/settings.py
+++ perso/settings.py
@@ -149,11 +149,16 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            #'formatter': 'simple',
+        },
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
+            'handlers': ['mail_admins', 'console'],
             'level': 'ERROR',
             'propagate': True,
         },
