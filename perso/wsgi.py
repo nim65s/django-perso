a62584c75ed33b1c9332cbcc54a7a12ff9f80439
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-11-30 11:32:26 +0100

    fix wsgi

--- perso/wsgi.py
+++ perso/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test1_6.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "perso.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
