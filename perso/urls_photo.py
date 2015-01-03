12c9ebd14397a45d364e6147078b38d725abe0e2
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-01-03 04:41:39 +0100

    photologue: juste des galleries, et ajout des templates

--- perso/urls_photo.py
+++ perso/urls_photo.py
@@ -1,4 +1,4 @@
-from photologue.models import Gallery, Photo
+from photologue.models import Gallery
 
 from django.conf.urls import patterns, url
 from django.views.generic import DetailView, ListView
@@ -6,5 +6,4 @@ from django.views.generic import DetailView, ListView
 urlpatterns = patterns('',
         url(r'^albums/$', ListView.as_view(queryset=Gallery.objects.is_public()), name='gallery-list'),
         url(r'^album/(?P<slug>[\-\d\w]+)/$', DetailView.as_view(model=Gallery), name='pl-gallery'),
-        url(r'^(?P<slug>[\-\d\w]+)/$', DetailView.as_view(model=Photo), name='pl-photo'),
         )
