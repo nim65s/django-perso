2ca72f60f237466898d9c2d924eb0fad617a28b4
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-01-10 22:11:25 +0100

    home

--- perso/urls.py
+++ perso/urls.py
@@ -11,12 +11,15 @@ urlpatterns = [
 
         url(r'^cine/', include('cine.urls')),
         url(r'^comptes/', include('comptes.urls')),
-        url(r'^gpg/(?P<url>.*)$', RedirectView.as_view(url='/PGP/%(url)s', permanent=True)),
+        url(r'^gpg/(?P<url>.*)$', RedirectView.as_view(url='/PGP/%(url)s',
+            permanent=True)),
         url(r'^PGP/', include('pgp_tables.urls')),
         url(r'^groupe/', include('groupe.urls')),
 
         url(r'^admin/', admin.site.urls),
         url(r'^photo/', include('perso.urls_photo')),
         url(r'^cgi', permission_denied),
+        url(r'^home$', RedirectView.as_view(url=reverse_lazy('dmdb:blog'),
+            permanent=True), name="home"),
         url(r'', include('dmdb.urls')),
         ]
