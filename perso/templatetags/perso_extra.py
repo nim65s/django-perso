f8048aedeb178f35f084a771a291d1132d8b9c4a
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-08-14 20:13:21 +0200

    photo detail

--- perso/templatetags/perso_extra.py
+++ perso/templatetags/perso_extra.py
@@ -1,7 +1,21 @@
 from django import template
+from django.utils.http import urlquote
 from django.utils.safestring import mark_safe
 
 register = template.Library()
+
+EXIF_DATA = [
+    ('Image Make', 'Constructeur'),
+    ('Image Model', 'Appareil Photo'),
+    ('EXIF BrightnessValue', 'Luminosité'),
+    ('EXIF ExposureTime', 'Temps d’exposition'),
+    ('EXIF FocalLength', 'Longueur Focale'),
+    ('EXIF ShutterSpeedValue', 'Vitesse d’obturateur'),
+    ('EXIF ISOSpeedRatings', 'Vitesse ISO'),
+    ('EXIF SceneCaptureType', 'Type de scène'),
+    ('EXIF WhiteBalance', 'Balance des blancs'),
+    ('EXIF MeteringMode', 'Metering'),
+]
 
 
 @register.simple_tag
@@ -20,3 +34,22 @@ def email(email, request):
         at, dot = ('<span class="%s"></span>' % i for i in ['at', 'dot'])
         content = email.replace('@', at).replace('.', dot)
     return mark_safe('<span class="mail">%s</span>' % content)
+
+@register.simple_tag
+def exif(photo):
+    tags = photo.EXIF()
+    ret = ['<dl class="dl-horizontal">']
+    for key, val in EXIF_DATA:
+        if key in tags:
+            ret.append('<dt>%s:</dt>' % val)
+            ret.append('<dd>%s</dd>' % tags[key])
+    if 'GPS GPSLatitude' in tags and 'GPS GPSLongitude' in tags:
+        lat = '''%i°%i'%i"''' % tuple(i.num / i.den for i in tags['GPS GPSLatitude'].values)
+        lat += tags['GPS GPSLatitudeRef'].values
+        lon = '''%i°%i'%i"''' % tuple(i.num / i.den for i in tags['GPS GPSLongitude'].values)
+        lon += tags['GPS GPSLongitudeRef'].values
+        url = 'www.google.fr/maps/place/%s, %s' % (lat, lon)
+        ret.append('<dt>Coordonnées approximatives:</dt>')
+        ret.append('<dd><a href="https://%s">%s, %s</a></dd>' % (urlquote(url), lat, lon))
+    ret.append('<dl>')
+    return mark_safe('\n'.join(ret))
