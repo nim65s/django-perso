0f53128ee05276ad0744f6ed4d5c4298783e9442
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-09-16 23:37:01 +0200

    ndh email filter

--- perso/templatetags/perso_extra.py
+++ perso/templatetags/perso_extra.py
@@ -26,15 +26,6 @@ def url_get(request, key=None, value=None):
     return "?%s" % get.urlencode()
 
 
-@register.filter
-def email(email, request):
-    if request.user.is_authenticated():
-        content = '<a href="mailto:%s">%s</a>' % (email, email)
-    else:
-        at, dot = ('<span class="%s"></span>' % i for i in ['at', 'dot'])
-        content = email.replace('@', at).replace('.', dot)
-    return mark_safe('<span class="mail">%s</span>' % content)
-
 @register.simple_tag
 def exif(photo):
     tags = photo.EXIF()
